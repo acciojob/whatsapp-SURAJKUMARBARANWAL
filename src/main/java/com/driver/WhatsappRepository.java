554b3671b4e113263829adb016b3eef8589c4e94
Suraj Kumar <surajbaranwal56@gmail.com>
2023-01-28 16:01:15 +0530

    fir commit

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -17,6 +17,7 @@ public class WhatsappRepository {
     private int customGroupCount;
     private int messageId;
 
+
     public WhatsappRepository(){
         this.groupMessageMap = new HashMap<Group, List<Message>>();
         this.groupUserMap = new HashMap<Group, List<User>>();
@@ -25,5 +26,76 @@ public class WhatsappRepository {
         this.userMobile = new HashSet<>();
         this.customGroupCount = 0;
         this.messageId = 0;
+
+    }
+    public String createUser(String  name,String mobileNum){
+        if(userMobile.contains(mobileNum)){
+            return "mobile is register, already";
+        }
+        userMobile.add(mobileNum);
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users) {
+        int n= users.size();
+        if(n==2){
+            //private chat
+            Group group=new Group(users.get(1).getName(),n);
+            groupUserMap.put(group,users);
+            adminMap.put(group,users.get(0));
+            groupMessageMap.put(group,new ArrayList<>());
+            return group;
+        }
+        else if(n>2){
+            //group is more than 2 members
+            Group group=new Group( "Group "+(++customGroupCount),n);
+            groupUserMap.put(group,users);
+            adminMap.put(group,users.get(0));
+            groupMessageMap.put(group,new ArrayList<>());
+            return group;
+        }
+        return null;
+    }
+
+    public int createMessage(String content) {
+        Message msg=new Message(content);
+        msg.setId(++messageId);
+        msg.setTimestamp(new Date());
+        return msg.getId();
+    }
+
+
+    public int sendMessage(Message message, User sender, Group group) {
+      if(!groupUserMap.containsKey(group)){
+          //-1 group does not exist
+          return -1;
+      }
+      else if(!groupUserMap.get(group).contains(sender)){
+          //user is not member in that group
+          return -2;
+      }
+      else{
+          //this sender is in the group
+          senderMap.put(message,sender);
+          groupMessageMap.get(group).add(message);
+          return groupMessageMap.get(group).size();
+      }
+    }
+
+    public String changeAdmin(User approver, User user, Group group) {
+        if(!groupUserMap.containsKey(group)){
+            return  "group not find";
+        }
+        else if(adminMap.get(group).equals(approver)==false){
+            return "Approver is not admin";
+        }
+        else if(groupUserMap.get(group).contains(user)==false){
+            return "User not found in group";
+        }
+        else {
+            //now change
+            adminMap.put(group,user);
+            return "SUCCESS";
+        }
     }
 }
